9678a3321db9fe03aec3e5451ec0070a6bcd9b2d
gilang aditya <madegilangaditya32@gmail.com>
2022-11-08 23:40:16 +0800

    add js script on mark all as read button

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,12 @@
 console.log('masuk');
 
 
-const items = document.querySelectorAll('.notification-item');
-const unread = document.querySelector('.mark-read');
-unread.addEventListener('click', unreadEvent(items));
 
-const unreadEvent = (items)=>{
+const unread = document.querySelector('.mark-read');
+
+
+const unreadEvent = ()=>{
+    const items = document.querySelectorAll('.notification-item');
     items.forEach((item) => {
         if(item.classList.contains('unread')){
             item.classList.remove('unread')
