89ba1bfd08acad7390c0c25a681735217ea5cf28
gilang aditya <madegilangaditya32@gmail.com>
2022-12-27 16:58:07 +0800

    add js style for close notification each click

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,14 +3,48 @@ console.log('masuk');
 
 
 const unread = document.querySelector('.mark-read');
+const unreadItems = document.querySelectorAll('.notification-item.unread');
+
 
 
 const unreadEvent = ()=>{
     const items = document.querySelectorAll('.notification-item');
+    const notifCount = document.querySelector('.header-wrap span');
     items.forEach((item) => {
         if(item.classList.contains('unread')){
             item.classList.remove('unread')
         }
-        console.log(item);
     });
+    notifCount.style.display ='none';
+}
+
+unreadItems.forEach((unreadItem)=>{
+    unreadItem.addEventListener('click', unreadClick);
+});
+
+// unreadItems.forEach((unreadItem)=>{
+//     unreadItem.addEventListener('click', function(){
+//         let count = parseInt(document.querySelector('.header-wrap span').textContent);
+//         console.log(count);
+//         unreadItem.classList.remove('unread');
+//         count -= 1;
+//         if(count == 0){
+//             document.querySelector('.header-wrap span').style.display ='none';
+//         }else{
+//             document.querySelector('.header-wrap span').textContent = count;
+//         }
+//     });
+// });
+
+function unreadClick(){
+    let count = parseInt(document.querySelector('.header-wrap span').textContent);
+    console.log(count);
+    this.classList.remove('unread');
+    count -= 1;
+    if(count == 0){
+        document.querySelector('.header-wrap span').style.display ='none';
+    }else{
+        document.querySelector('.header-wrap span').textContent = count;
+    }
+    
 }
