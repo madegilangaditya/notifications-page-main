394d29138838d7a9935f7d7124d5d96f0ca05139
gilang aditya <madegilangaditya32@gmail.com>
2023-02-15 21:40:41 +0800

    add desktop styling

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,5 @@
-console.log('masuk');
-
-
-
 const unread = document.querySelector('.mark-read');
 const unreadItems = document.querySelectorAll('.notification-item.unread');
-
-
 
 const unreadEvent = ()=>{
     const items = document.querySelectorAll('.notification-item');
@@ -21,20 +15,6 @@ const unreadEvent = ()=>{
 unreadItems.forEach((unreadItem)=>{
     unreadItem.addEventListener('click', unreadClick);
 });
-
-// unreadItems.forEach((unreadItem)=>{
-//     unreadItem.addEventListener('click', function(){
-//         let count = parseInt(document.querySelector('.header-wrap span').textContent);
-//         console.log(count);
-//         unreadItem.classList.remove('unread');
-//         count -= 1;
-//         if(count == 0){
-//             document.querySelector('.header-wrap span').style.display ='none';
-//         }else{
-//             document.querySelector('.header-wrap span').textContent = count;
-//         }
-//     });
-// });
 
 function unreadClick(){
     let count = parseInt(document.querySelector('.header-wrap span').textContent);
